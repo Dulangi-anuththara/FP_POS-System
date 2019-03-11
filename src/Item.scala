5d617511fd289da7d1734e16b7100e1805b84803
damsarar <damsarar@gmail.com>
2019-03-12 01:14:26 +0530

    Initial Commit

--- src/Item.scala
+++ src/Item.scala
@@ -3,6 +3,8 @@ class Item(val id:Int, val name:String, val price:Double) {
     println("Item ID : "+id)
     println("Item Name : "+name)
     println("Item Price : Rs. "+price)
+
+    //print the item details
   }
 
 }
