9a895c8fd1d4a02dfdbf689f6661d080345e0a9d
damsarar <damsarar@gmail.com>
2019-03-12 00:52:56 +0530

    Initial Commit

---
+++ src/Driver.scala
@@ -0,0 +1,7 @@
+object Driver {
+  def main(args:Array[String]): Unit ={
+    val item1=new Item(1,"Pen", 20.00)
+    item1.printDetails()
+  }
+
+}
---
+++ src/Item.scala
@@ -0,0 +1,8 @@
+class Item(val id:Int, val name:String, val price:Double) {
+  def printDetails()={
+    println("Item ID : "+id)
+    println("Item Name : "+name)
+    println("Item Price : Rs. "+price)
+  }
+
+}
