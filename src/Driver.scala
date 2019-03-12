d8ab9cd928409f9c15d241997a1b328cef3b8b85
damsarar <damsarar@gmail.com>
2019-03-12 22:32:11 +0530

    Test commit

--- src/Driver.scala
+++ src/Driver.scala
@@ -1,6 +1,12 @@
 object Driver {
   def main(args:Array[String]): Unit ={
-    val item1=new Item(1,"Pen", 20.00)
+    println("Enter an Item ID")
+    val a:Int=scala.io.StdIn.readInt()
+    println("Enter an Item Name")
+    val b:String=scala.io.StdIn.readLine()
+    println("Enter an Item Price")
+    val c:Int=scala.io.StdIn.readInt()
+    val item1=new Item(a,b,c)
     item1.printDetails()
   }
 
--- src/Item.scala
+++ src/Item.scala
@@ -1,10 +1,17 @@
-class Item(val id:Int, val name:String, val price:Double) {
+class Item() {
+  val id:Int=1
+  val name:String="abc"
+  val price:Double=1.0
   def printDetails()={
     println("Item ID : "+id)
     println("Item Name : "+name)
     println("Item Price : Rs. "+price)
 
     //print the item details
+  }
+
+  def addItem(val id:Int, val name:String, val price:Double)={
+
   }
 
 }
