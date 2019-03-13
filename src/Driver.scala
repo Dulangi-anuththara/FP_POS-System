0261924b36353d1ba4f0a7410fad7768b603c7f7
damsarar <damsarar@gmail.com>
2019-03-13 13:54:07 +0530

    6th Commit

--- src/Driver.scala
+++ src/Driver.scala
@@ -1,19 +1,41 @@
 object Driver {
   def main(args:Array[String]): Unit ={
-    println("Enter the Item ID :")
-    val id:Int=scala.io.StdIn.readInt()
-    println("Enter the Item Name :")
-    val name:String=scala.io.StdIn.readLine()
-    println("Enter the Item Price :")
-    val price:Double=scala.io.StdIn.readDouble()
-    println("Enter the Item Quantity :")
-    val quantity:Int=scala.io.StdIn.readInt()
 
-    val item1=new Inventry()
+    val inventry=new Inventry()
 
-    item1.addItems(id,name,price,quantity)
-    //item1.printDetails()
-    
+
+    println("Select an option :")
+    println("1 - Add an item to the inventory")
+    println("2 - Delete item from the inventory")
+
+    var opt:Int=scala.io.StdIn.readInt()
+
+
+    opt match {
+      case 1 =>
+        println("Enter the Item ID :")
+        val id:Int=scala.io.StdIn.readInt()
+        println("Enter the Item Name :")
+        val name:String=scala.io.StdIn.readLine()
+        println("Enter the Item Price :")
+        val price:Double=scala.io.StdIn.readDouble()
+        println("Enter the Item Quantity :")
+        val quantity:Int=scala.io.StdIn.readInt()
+
+
+
+        inventry.addItems(id,name,price,quantity)
+
+        println("Item added successfully")
+
+      case 2 => println("Item purchased")
+
+      case _ => println("Invalid option")
+
+    }
+
+
+
   }
 
 }
--- src/Inventry.scala
+++ src/Inventry.scala
@@ -1,18 +1,42 @@
 class Inventry {
-    def purchaseItem()={
+
+
+
+    def addItem1(itemId:Int, itemName:String, itemPrice:Double, itemQuantity:Int):Unit ={
+        val item=new Item(itemId,itemName,itemPrice,itemQuantity)
+
+    }
+
+    def addItem2(itemId:Int, itemName:String, itemPrice:Double, itemQuantity:Int):Unit ={
+        val item=new Item(itemId,itemName,itemPrice,itemQuantity)
+
+    }
+
+    def addItem3(itemId:Int, itemName:String, itemPrice:Double, itemQuantity:Int):Unit ={
+        val item=new Item(itemId,itemName,itemPrice,itemQuantity)
+
+    }
+
+    def deleteItem1()={
         println("Enter Item ID to purchase : ")
         val getId:Integer=scala.io.StdIn.readInt()
         println("Enter Item Quantity to purchase : ")
         val getQuantity:Integer=scala.io.StdIn.readInt()
     }
 
-    def addItems(itemId:Int, itemName:String, itemPrice:Double, itemQuantity:Int):Unit ={
-        val item=new Item(itemId,itemName,itemPrice,itemQuantity)
-
+    def deleteItem2()={
+        println("Enter Item ID to purchase : ")
+        val getId:Integer=scala.io.StdIn.readInt()
+        println("Enter Item Quantity to purchase : ")
+        val getQuantity:Integer=scala.io.StdIn.readInt()
     }
 
-    def printDetails()={
 
+    def deleteItem3()={
+        println("Enter Item ID to purchase : ")
+        val getId:Integer=scala.io.StdIn.readInt()
+        println("Enter Item Quantity to purchase : ")
+        val getQuantity:Integer=scala.io.StdIn.readInt()
     }
 
 }
